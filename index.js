879e774906d96a798434e425f02f0c1e2807df7f
Lancelot fish <flegact60132@gmail.com>
2024-03-05 13:17:45 +0800

    2024/03/05

--- index.js
+++ index.js
@@ -33,5 +33,10 @@ function handleEvent(event) {
         }],
     });
 }
+// define the first route
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));

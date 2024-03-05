a1f621784f37b99d08c098505198ffd1d317a285
Lancelot fish <flegact60132@gmail.com>
2024-03-05 12:51:57 +0800

    2024/03/05

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-    channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
-    channelSecret: 'YOUR_CHANNEL_SECRET'
+    channelAccessToken: 'RdFcsMBl+Anxfn8q/y1BCXpgnr5w4s9arTGLjZICFwKY4l0EMSJiMdUiSbfWq9v3O7Tc5mM6BMLpWZszUYgfJ+xCkA9mqiHfcZ4gtffrrBq95aU+vY1KoSjuO2ENghYlDDkA+ceO7oe56FQeuQ3jwAdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'b3301e0a2252d81985541b0431d4c9b7'
 };
 
 const app = express();
@@ -14,7 +14,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 });
 
 const client = new line.messagingApi.MessagingApiClient({
-    channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+    channelAccessToken: 'RdFcsMBl+Anxfn8q/y1BCXpgnr5w4s9arTGLjZICFwKY4l0EMSJiMdUiSbfWq9v3O7Tc5mM6BMLpWZszUYgfJ+xCkA9mqiHfcZ4gtffrrBq95aU+vY1KoSjuO2ENghYlDDkA+ceO7oe56FQeuQ3jwAdB04t89/1O/w1cDnyilFU=',
 });
 function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
